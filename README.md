2a5372d54e57c69a83135f0a395931dd621f4331
Matthieu <mcormier@mail.preenandprune.com>
2013-04-08 16:25:28 +0000

    intial import

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-StGeorgeCalendar
-================
+# St. George Calendar Generate-tron-amajig #
+
+## Ubunutu installation Notes ##
+
+sudo apt-get update
+sudo apt-get install libxml2 libxml2-dev libxslt1-dev
+sudo gem install nokogiri
+sudo gem install google\_calendar
+
+
+## Rules ##
+- create one entry per day and put what you want to appear in the description
+- don't use recurring events because the parsing library breaks
---
+++ bin/getCalendarData.rb
@@ -0,0 +1,175 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'google_calendar'
+require 'nokogiri'
+require 'builder'
+require 'date'
+
+
+load '../config/config.properties'
+
+# reference: https://github.com/northworld/google_calendar
+
+class MyCal < Google::Calendar 
+
+  # By default Google only returns 25 results at a time.
+  # This allows us to grab everything in one go
+  def lookup()
+    event_lookup("?max-results=999")
+  end
+
+end
+
+def getCloudEventData
+
+  cal = MyCal.new(
+    :username => @username,
+    :password => @password,
+    :app_name => 'mycompany.com-googlecalendar-integration',
+    :calendar => @calendar
+  )
+
+  events = cal.lookup()
+
+  events
+end
+
+
+COMMON_YEAR_DAYS_IN_MONTH = [nil, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+#
+# The St. George Calendar runs from April to Oct
+# So we can use constants and not worry about leap
+# years in February
+#
+def getDaysInMonth(month)
+  if month == 2
+    raise "February and leap years not supported!!"
+  end 
+  COMMON_YEAR_DAYS_IN_MONTH[month]
+end
+
+
+def eventOnDay(event, day)
+    startTime = Time.parse(event.start_time)
+    endTime = Time.parse(event.end_time)
+
+    startDate = Date.parse(startTime.strftime('%Y/%m/%d'))
+    endDate = Date.parse(endTime.strftime('%Y/%m/%d'))
+
+    # All day events end at midnight on the next day
+    # which is totally fucking wrong
+    if event.all_day?
+      endDate = endDate.prev_day
+    end
+
+   return (day >= startDate and day <= endDate) 
+end
+
+
+def getEventString(currentDay, events)
+
+  eventString = ""
+
+  events.each do |event|
+    start = Time.parse(event.start_time)
+
+    if eventOnDay(event, currentDay)
+      # use the description
+      if eventString.length > 0 
+        eventString = eventString + "\n" 
+      end
+      eventString += event.content
+    end
+
+  end
+
+  eventString 
+end
+
+def generateMonth( firstDay, filename, events )
+  x = Builder::XmlMarkup.new( :indent => 2 )
+  # i.e. "April 2013"
+  monthHeader = Date::MONTHNAMES[firstDay.month] + " " + firstDay.year.to_s
+
+  currentDay = firstDay
+  currentMonth = firstDay.month
+
+  daysInMonth = getDaysInMonth(firstDay.month)
+  day = 1
+  
+  File.open( @outputDir + filename, "w") do |out|
+    out.puts x.div( :class => "month") {
+      outputHeaderAndWeekDays(x, monthHeader)
+
+     
+      while day < daysInMonth
+
+        saveCurrentDay = currentDay
+ 
+        x.div( :class => "dateRow") {
+
+          for i in 0..6
+            if currentDay.wday == i and currentDay.month == currentMonth
+              val = currentDay.day.to_s
+              currentDay = currentDay.next
+              cssClasses = ""
+            else
+              val = " "
+              cssClasses = "otherMonth"
+            end
+
+            x.div( val, :class => cssClasses )
+          end
+
+        }
+
+       currentDay = saveCurrentDay
+
+        x.div( :class => "contentRow") {
+
+          for i in 0..6
+            if currentDay.wday == i and currentDay.month == currentMonth
+              val = getEventString( currentDay, events)
+              currentDay = currentDay.next
+              cssClasses = "innerContainer"
+            else
+              val = ""
+              cssClasses = "innerContainer otherMonth"
+            end
+            
+            x.div( :class => "outerContainer") {
+                x.div( val, :class => cssClasses )
+            }
+            
+          end
+        }
+
+       day += 7
+
+    end
+
+    }
+  end
+end
+
+def outputHeaderAndWeekDays(x, monthHeader)
+      x.div( monthHeader, :class => "monthHeader" )  
+      x.div( :class => "weekDays") {
+        Date::DAYNAMES.each { |day|
+          x.div day
+        }
+      } 
+end
+
+
+events = getCloudEventData
+puts "Got Event Data"
+
+#d = Date.parse ('1 Apr ' + @year)
+#generateMonth(d, "april.html", events)
+#puts "Generated April"
+
+d = Date.parse ('1 May ' + @year)
+generateMonth(d, "may.html", events)
+puts "Generated May"
