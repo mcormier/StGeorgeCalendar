d2efb37f57e8a7f498becc01a6cbf7f176456a46
Matthieu Cormier <mcormier@cs.dal.ca>
2015-03-09 19:49:54 -0300

    Wrap the event with occurrences in a new object.

--- bin/iCalParser.rb
+++ bin/iCalParser.rb
@@ -22,21 +22,24 @@ calendar = calendars[0]
 # Returns an array of Icalendar::Event
 events = calendar.events
 
+pp_events = Array.new
+
 events.each do |event|
+
+  occurrences = event.occurrences_between(Date.parse('2015-01-01'), Date.parse('2015-12-01'))
+  pp_event = PPEvent.new(event, occurrences)
+  pp_events.push pp_event
+
   if event.summary == 'TGIF Tennis'
-  puts event.summary
-  puts "DTSTART: #{event.dtstart}"
-  puts "DTEND: #{event.dtstart}"
+    puts event.summary
+    puts "DTSTART: #{event.dtstart}"
+    puts "DTEND: #{event.dtstart}"
+    puts "occurrences: #{occurrences.length}"
+    puts ''
 
-  occurrences = event.occurrences_between(Date.parse('2015-03-01'), Date.parse('2015-09-01'))
-  puts "occurrences: #{occurrences.length}"
-  puts ''
-
-
-  occurrences.each do |occurs|
-    puts "#{occurs.start_time}"
-  end
-
+    occurrences.each do |occurs|
+      puts "#{occurs.start_time}"
+    end
   end
 
 end
