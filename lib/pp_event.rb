3ba5cd542f52e01ee700a96e0c4b79679002b66d
Matthieu Cormier <mcormier@cs.dal.ca>
2015-03-09 20:44:22 -0300

    Implement first cut of on_day?

--- lib/pp_event.rb
+++ lib/pp_event.rb
@@ -7,6 +7,20 @@ class PPEvent
   end
 
   def on_day?(day)
+
+    if @occurrences.length > 0
+      # TODO loop through occurrences and return true if start_time end_time works...
+
+      @occurrences.each do |occurs|
+        if day >= occurs.start_time.to_date and day <= occurs.end_time.to_date
+          return true
+        end
+      end
+
+    end
+
+    # TODO -- if 0 occurrences need other logic.
+
     #(day >= self.start_date and day <= self.end_date )
     false
   end
