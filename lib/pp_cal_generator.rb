1eb3eb467ce1187130d4a16dae8889803dc37315
Matthieu Cormier <mcormier@cs.dal.ca>
2015-03-09 20:28:07 -0300

    add generate stub.

--- lib/pp_cal_generator.rb
+++ lib/pp_cal_generator.rb
@@ -5,4 +5,16 @@ class PPCalGenerator
      @events = events
   end
 
+
+  def generate
+
+    for i in 4..11
+      month_name = Date::MONTHNAMES[i]
+      puts "TODO generate #{month_name} "
+    end
+
+  end
+
+
+
 end
