1fd41cfc4d6aa0bd92fa1965cb2e35e651f187a2
Matthieu Cormier <mcormier@cs.dal.ca>
2015-03-10 22:26:52 -0300

    Add safe require.

--- lib/stgeorgecal.rb
+++ lib/stgeorgecal.rb
@@ -1,5 +1,21 @@
-require 'icalendar'
+
+def safe_require( gemName, gemVersion )
+
+  begin
+    gem gemName, "=#{gemVersion}"
+    require gemName
+  rescue LoadError
+    puts " Missing #{gemName} gem"
+    puts " Try: gem install #{gemName} -v #{gemVersion}"
+    exit
+  end
+
+end
+
+safe_require('icalendar', '2.2.0')  # https://github.com/icalendar/icalendar
+
 require 'icalendar/recurrence'
+
 require 'builder'
 require 'date'
 
